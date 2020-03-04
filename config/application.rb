57f1443287fb977a593ae38586407eeccb92ebe5
Sergio Torres <sergio.uft@gmail.com>
2020-03-04 13:02:48 -0300

    feature(cors): Add origin

--- config/application.rb
+++ config/application.rb
@@ -36,7 +36,7 @@ module BooksApi
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins '*'
+        origins 'https://bookstore.herokuapp.com/'
         resource(
           '*',
           headers: :any,
