009d70a11f414e58ce427b6eb9dc63ddf1b49946
Sergio Torres <sergio.uft@gmail.com>
2020-03-04 13:14:04 -0300

    fix(cors): Fix origin to right client address

--- config/application.rb
+++ config/application.rb
@@ -36,12 +36,8 @@ module BooksApi
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins '*'
-        resource(
-          '*',
-          headers: :any,
-          methods: [:get, :put, :delete, :post, :options]
-        )
+        origins 'https://ssf-bookstore.herokuapp.com/'
+        resource '/api/', headers: :any, methods: [:get, :put, :delete, :post, :options]
       end
     end
   end
